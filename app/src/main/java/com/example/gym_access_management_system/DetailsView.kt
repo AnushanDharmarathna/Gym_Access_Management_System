18c6b9f9cb1e30063dd4fbc15ef310e03ff4a7b6
AnushanDharmarathna <haianushan@gmail.com>
2024-04-21 23:03:28 +0530

    make Details view interface

---
+++ app/src/main/java/com/example/gym_access_management_system/DetailsView.kt
@@ -0,0 +1,11 @@
+package com.example.gym_access_management_system
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_view)
+    }
+}
